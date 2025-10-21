fead000ffb84ebc9526c9db7086a88084b6a7a23
danielpbittner <fakeemail@gmail.com>
2025-10-20 20:09:30 -0500

    Removed Personal Key

--- Spoonacular_api.py
+++ Spoonacular_api.py
@@ -1,12 +1,10 @@
-# TODO: Make requests to the Spoonacular API
+# TODO: Make requests to the Spoonacular API 
 
 import requests
 import os
 
 API_KEY = os.getenv("SPOONACULAR_API_KEY")
 URL = "https://api.spoonacular.com/recipes/complexSearch"
-
-def gemini_query
 
 def get_diet_recipe_by_meal(meal):
     params = {

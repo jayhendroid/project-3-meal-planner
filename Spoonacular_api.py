6b651f8a111e9715dd863f3f19401ed80a798ba2
danielpbittner <fakeemail@gmail.com>
2025-10-27 18:46:24 -0500

    forgot to add changes to function get_diet_recipes_by_meal

--- Spoonacular_api.py
+++ Spoonacular_api.py
@@ -8,11 +8,10 @@ URL = "https://api.spoonacular.com/recipes/complexSearch"
 
 # Get diet recipes by meal name from Spoonacular API
 # 'meal' will come from user input in the Flask app
-def get_diet_recipes_by_meal(meal, dietStatus, intolerances):
+def get_diet_recipes_by_meal(meal, intolerances):
     params = {
     'apiKey': API_KEY,
     'query': meal,
-    'diet': dietStatus,
     'intolerances': intolerances,
     'number': 5  # Request 5 recipes
     }
@@ -29,5 +28,5 @@ def get_diet_recipes_by_meal(meal, dietStatus, intolerances):
     return mealdata['results']
 
 
-# TODO: the Query field in the above function will be populated by user input from the Flask app
+# TODO: the 'params' fields in the above function will be populated by user input from the Flask app
 # This can be changed to include diet preferences, as well as other parameters like cuisine, intolerances, etc.

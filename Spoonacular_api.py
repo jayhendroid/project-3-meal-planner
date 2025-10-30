3fc494a745630bf44c6755ae7485ab03f98ab563
danielpbittner <fakeemail@gmail.com>
2025-10-30 13:21:56 -0500

    ensured the API.py will talk to the Gemini.py

--- Spoonacular_api.py
+++ Spoonacular_api.py
@@ -7,7 +7,7 @@ API_KEY = os.getenv("SPOONACULAR_API_KEY")
 URL = "https://api.spoonacular.com/recipes/complexSearch"
 
 # Get diet recipes by meal name from Spoonacular API
-# 'meal' will come from user input in the Flask app
+# 'meal', 'maxCalores', etc will come from user input in the Flask app
 def get_diet_recipes_by_meal(meal, intolerances, calorieTarget, diet):
     params = {
     'apiKey': API_KEY,
@@ -19,17 +19,8 @@ def get_diet_recipes_by_meal(meal, intolerances, calorieTarget, diet):
     }
 
     response = requests.get(URL, params=params)
-    mealdata = response.json()
-    
-    # Process the JSON response
-    for recipe in mealdata['results']:
-        print(f"Title: {recipe['title']}")
-        print(f"Recipe ID: {recipe['id']}")
-        print(f"Calories: {recipe.get('calories', 'N/A')}")
-        print(f"Diet: {diet}")
-        print("-" * 20)
-
-    return mealdata['results']
+    recipes = response.json()  # Get the JSON response
+    return recipes['results']
 
 
 # TODO: the 'params' fields in the above function will be populated by user input from the Flask app

79f728b0b6b52fb6fdf1a71ceeaf7ec6d409fe5c
danielpbittner <fakeemail@gmail.com>
2025-10-30 16:27:30 -0500

    added unit tests for mealRestrictions

--- UnitTests.py
+++ UnitTests.py
@@ -11,17 +11,61 @@ def userMealRestrictions(intolerences):
 
 # Unit tests for userMealRestrictions function
 class TestMealRestrictions(unittest.TestCase):
-    def test_vegan_restriction(self):
-        self.assertTrue(userMealRestrictions('vegan'))
+    def test_valid_restriction(self):
+        self.assertTrue(userMealRestrictions('vegan', 'gluten-free', 'vegetarian'))
 
     def test_non_restricted_meal(self):
         self.assertFalse(userMealRestrictions('chicken'))
 
-    def test_gluten_free_restriction(self):
-        self.assertTrue(userMealRestrictions('gluten-free'))
-
     def test_empty_string(self):
         self.assertFalse(userMealRestrictions(''))
+
+    def test_case_insensitivity(self):
+        self.assertTrue(userMealRestrictions('Vegan'))
+
+    def test_partial_match(self):
+        self.assertFalse(userMealRestrictions('veg'))
+
+    def test_numeric_input(self):
+        self.assertFalse(userMealRestrictions('1234'))
+
+    def test_special_characters(self):
+        self.assertFalse(userMealRestrictions('@#$%'))
+
+    def test_multiple_restrictions(self):
+        self.assertTrue(userMealRestrictions('vegetarian, gluten-free'))
+
+    def test_whitespace_input(self):
+        self.assertFalse(userMealRestrictions('   '))
+
+    def test_none_input(self):
+        self.assertFalse(userMealRestrictions(None))
+
+    def test_long_string_input(self):
+        long_string = 'a' * 1000
+        self.assertFalse(userMealRestrictions(long_string))
+
+    def test_dictionary_input(self):
+        self.assertFalse(userMealRestrictions({'diet': 'vegan'}))
+
+    def test_list_input(self):
+        self.assertFalse(userMealRestrictions(['vegan', 'vegetarian']))
+
+    def test_boolean_input(self):
+        self.assertFalse(userMealRestrictions(True))
+        
+
+# Unit tests for Flask app routes
+
+# Units tests for recipe_info function in Gemini.py
+
+# Unit tests for system_prompt function in Gemini.py
+
+# Unit tests for get_diet_recipes_by_meal function in Spoonacular_api.py
+
+# Unit tests for get_recipe_information function in Spoonacular_api.py
+
+# Additional unit tests can be added here as needed
 
 if __name__ == '__main__':
     unittest.main()
